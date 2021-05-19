017d266b960b649eb4b891096b5fc09758f5d7b0
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-05-19 12:13:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# naredba za instalaciju, pokrenuti u terminalu:
+# pip install -r requirements.txt
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
